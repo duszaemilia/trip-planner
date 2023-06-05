8403e27ea4964cce746e7280f944936a4848b0ff
duszaemilia <dusza.emilia@gmail.com>
2023-06-06 00:07:36 +0200

    posting data to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,82 @@
 import './App.css'
 import {useEffect, useState} from "react";
 import {getAllTrips} from "./helpers/api.jsx";
+import {sendTripData} from "./helpers/api.jsx";
 
 function App() {
 
-    const [trips,setTrips] = useState([]);
+    const [trips, setTrips] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
-useEffect(() =>{
-    getAllTrips()
-        .then((data) => {
-        setTrips(data)
-    })
-        .catch(console.error)
-},[])
+    useEffect(() => {
+        getAllTrips()
+            .then((data) => {
+                setTrips(data)
+            })
+            .catch(console.error)
+    }, [])
 
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const result = await sendTripData({
+            title, description, status:"open", startDate, endDate
+        });
+        setTitle("");
+        setDescription("");
+        setStartDate("");
+        setEndDate("");
+        setTrips([...trips,result])
+    }
 
     return (
         <>
+            <form onSubmit={handleSubmit}>
+
+                <div>
+                    <label htmlFor="title">Trip title</label>
+                    <input
+                        value={title}
+                        type="text"
+                        id="title"
+                        name="title"
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="title">Trip description</label>
+                    <textarea
+                        value={description}
+                        id="desc"
+                        name="desc"
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="startDate">Start Date</label>
+                    <input
+                        value={startDate}
+                        type="date"
+                        id="startDate"
+                        name="startDate"
+                        onChange={(event) => setStartDate(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="endDate">End Date</label>
+                    <input
+                        value={endDate}
+                        type="date"
+                        id="endDate"
+                        name="endDate"
+                        onChange={(event) => setEndDate(event.target.value)}
+                    />
+                </div>
+                <button type="submit">Save trip</button>
+            </form>
 
         </>
     )
--- src/helpers/api.jsx
+++ src/helpers/api.jsx
@@ -2,3 +2,18 @@ export async function getAllTrips() {
     const response = await fetch('http://localhost:3000/trips');
     return response.json()
 }
+
+
+export  async function sendTripData(data) {
+    const response = await fetch(
+        'http://localhost:3000/trips',
+        {
+            headers:{
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body:JSON.stringify(data)
+        }
+    )
+    return response.json()
+}
