5317dbe877847e50a0423ce861745ecb60f3f3c2
duszaemilia <dusza.emilia@gmail.com>
2023-06-06 23:56:36 +0200

    mui style installing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import './App.css'
 import {useEffect, useState} from "react";
 import {deleteTripAPI, getAllTrips, sendDataAPI} from "./helpers/api.jsx";
 import * as PropTypes from "prop-types";
 import {TripDetail} from "./components/TripDetail.jsx";
+import {Container} from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+
 
 TripDetail.propTypes = {
     trip: PropTypes.any,
@@ -33,7 +41,7 @@ function App() {
         event.preventDefault();
         const result = await sendDataAPI({
             title, description, status: "open", startDate, endDate
-        },"trips");
+        }, "trips");
         setTitle("");
         setDescription("");
         setStartDate("");
@@ -50,59 +58,66 @@ function App() {
 
 
     return (
-        <>
+        <Container maxWidth="md">
+            <h1>Trip Planner</h1>
             <form onSubmit={handleSubmit}>
 
-                <div>
-                    <label htmlFor="title">Trip title</label>
-                    <input
+
+                <Stack spacing={2} direction="column">
+                    <TextField
+                        label="Trip title"
+                        variant="outlined"
                         value={title}
                         type="text"
                         id="title"
                         name="title"
                         onChange={(event) => setTitle(event.target.value)}
                     />
-                </div>
-                <div>
-                    <label htmlFor="title">Trip description</label>
-                    <textarea
+
+
+                    <TextField
+                        label="Trip description"
+                        variant="outlined"
                         value={description}
                         id="desc"
                         name="desc"
-                        onChange={(event) => setDescription(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="startDate">Start Date</label>
-                    <input
-                        value={startDate}
-                        type="date"
-                        id="startDate"
-                        name="startDate"
-                        onChange={(event) => setStartDate(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="endDate">End Date</label>
-                    <input
-                        value={endDate}
-                        type="date"
-                        id="endDate"
-                        name="endDate"
-                        onChange={(event) => setEndDate(event.target.value)}
-                    />
-                </div>
-                <button type="submit">Save trip</button>
+                        onChange={(event) => setDescription(event.target.value)}/>
+
+                    <div>
+
+                        <label htmlFor="startDate">Start Date</label>
+                        <input
+                            value={startDate}
+                            type="date"
+                            id="startDate"
+                            name="startDate"
+                            onChange={(event) => setStartDate(event.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="endDate">End Date</label>
+                        <input
+                            value={endDate}
+                            type="date"
+                            id="endDate"
+                            name="endDate"
+                            onChange={(event) => setEndDate(event.target.value)}
+                        />
+                    </div>
+                    <Button variant="contained" type="submit">Save trip</Button>
+                </Stack>
             </form>
 
-            <section>
+
+            <Grid container spacing={2} gap={2} style={{marginTop:'20px'}}>
+
                 {trips.map((trip) => (
                     <TripDetail key={trip.id} trip={trip} noteId={noteId} onClick={() => setNoteId(trip.id)}
                                 onClick1={handleDeleteTrip}/>
                 ))}
-            </section>
+            </Grid>
 
-        </>
+        </Container>
     )
 }
 
