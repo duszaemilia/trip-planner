89a6798209cf21f19741a16ad963e149e9007eb1
duszaemilia <dusza.emilia@gmail.com>
2023-06-07 22:32:29 +0200

    Repair of drop-down list for trip plans

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 
 function TripCard(props) {
+    const [trip, setTripID] = useState({});
     const {tripId} = useParams();
+
+    useEffect(()=>{
+        getTripNote(tripId).then((trip) => setTripID(trip));
+    },[])
+    async function getTripNote(tripId) {
+        const response = await fetch(`http://localhost:3000/notes/${tripId}`);
+        return await response.json()
+    }
 
     return (
         <div> Trip details {tripId}</div>
--- src/components/TripDetails.jsx
+++ src/components/TripDetails.jsx
@@ -3,15 +3,25 @@ import Button from "@mui/material/Button";
 import Stack from '@mui/material/Stack';
 import {Container, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import List from "@mui/material/List";
-import InboxIcon from "@mui/icons-material/MoveToInbox.js";
+import InboxIcon from "@mui/icons-material/Inbox";
 import {ExpandLess, ExpandMore} from "@mui/icons-material";
 import Grid from '@mui/material/Unstable_Grid2';
-import React from "react";
+import Collapse from '@mui/material/Collapse';
 import {Link} from "react-router-dom";
+import React, {useState} from "react";
+
 import * as PropTypes from "prop-types";
 
 
 export default function TripDetails(props) {
+
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+
     return <Container maxWidth="md">
         <Grid xs={12}>
             <List
@@ -20,29 +30,40 @@ export default function TripDetails(props) {
                 aria-labelledby="nested-list-subheader"
 
             >
-                <ListItemButton onClick={() => {
-                }}>
+                <ListItemButton onClick={handleClick}>
                     <ListItemIcon>
                         <InboxIcon/>
                     </ListItemIcon>
-                    <ListItemText primary={props.trip.title} secondary={props.trip.description}/>
+                    <ListItemText primary={`${props.trip.title} from ${props.trip.startDate} to ${props.trip.endDate}`}
+                                  secondary={props.trip.description}/>
                     {open ? <ExpandLess/> : <ExpandMore/>}
                 </ListItemButton>
 
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton sx={{pl: 4}}>
+                            <ListItemIcon>
+
+                                <Stack spacing={4} gap={4} direction="row">
+                                    {props.noteId === props.trip.id ? (<AddNote tripId={props.trip.id}/>) : (
+                                        <Button onClick={props.onClick} variant="outlined">Add note</Button>
+                                    )}
+
+                                    <Link to={`/trip-card/${props.tripId}`}>
+                                        <Button variant="outlined">See all notes</Button>
+                                    </Link>
+                                    <Button onClick={props.onClick1} data-id={props.trip.id}
+                                            variant="outlined">Delete</Button>
+
+                                </Stack>
+
+                            </ListItemIcon>
+                        </ListItemButton>
+                    </List>
+                </Collapse>
             </List>
-            <Stack spacing={4} gap={4} direction="row">
-                {props.noteId === props.trip.id ? (<AddNote tripId={props.trip.id}/>) : (
-                    <Button onClick={props.onClick} variant="outlined">Add note</Button>
-                )}
 
-                <Link to={`/trip-card/${props.tripId}`} >
-                    <Button variant="outlined">See trip details</Button>
-                </Link>
-                <Button onClick={props.onClick1} data-id={props.trip.id} variant="outlined">Delete</Button>
-
-            </Stack>
         </Grid>
     </Container>;
 }
 
-// export default TripDetails;
